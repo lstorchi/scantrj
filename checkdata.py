d8898f8380032d202daf29947519d23859d1c462
Loriano Storchi <loriano@storchi.org>
2020-06-18 12:50:18 +0200

    backup

---
+++ checkdata.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+xls = pd.ExcelFile("./data/Fulldata.xlsx")
+data = pd.read_excel(xls)
+
+print(data.columns)
+
+#4204 site 1
+
+for name in data["name"]:
+    if name.find("4204") > 0:
+        print (data[data["name"] == name])
